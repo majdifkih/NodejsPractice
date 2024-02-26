62fb97f04548dcbe21b38bbe94a01a0ca7cec0bb
majdi <majdi@majdi.com>
2024-02-26 16:21:01 +0100

    connection mongodb

--- tp2nodejs/app.js
+++ tp2nodejs/app.js
@@ -1,15 +1,20 @@
 const express = require("express");
 const app = express();
-const port=5000;
 const voiture = require('./routes/voiture');
-
+const env = require('dotenv').config();
+const mongoose = require('mongoose');
 app.use("/voiture",voiture);
 
+mongoose.connect(process.env.MONGODB_URI).then(()=>{
+    console.log("connected to MongoDB");
+    app.listen(process.env.PORT,()=>{
+        console.log(`listening on port ${process.env.PORT}`);
+    
+    })
+}).catch((err)=>{
+    console.error('Error connecting to MongoDB', err.message);
+})
 
-app.listen(port,()=>{
-    console.log("listening on port 5000");
-
-});
 
 
 
