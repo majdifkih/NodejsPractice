822c14855e551ed50ddf27442634500a03469991
majdi <majdi@majdi.com>
2024-02-27 23:48:24 +0100

    model commit

--- tp1nodejs/model/user.js
+++ tp1nodejs/model/user.js
@@ -1,5 +1,5 @@
-const mongoose = require('mongoose'); // Import mongoose library
-const bcrypt = require('bcrypt'); // Import bcrypt library
+const mongoose = require('mongoose'); 
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
     username: {
@@ -16,18 +16,21 @@ const userSchema = new mongoose.Schema({
 // Hash the password before saving
 userSchema.pre('save', async function(next) {
     const user = this;
-    if (!user.isModified('password')) return next(); // If password is not modified, move to the next middleware
+    if (!user.isModified('password')) return next(); 
 
     try {
-        const salt = await bcrypt.genSalt(10); // Generate a salt with cost factor 10
-        const hashedPassword = await bcrypt.hash(user.password, salt); // Hash the password using the generated salt
-        user.password = hashedPassword; // Set the hashed password as the new password
-        next(); // Move to the next middleware
+        // Generate a salt with cost factor 10
+        const salt = await bcrypt.genSalt(10); 
+        // Hash the password using the generated salt
+        const hashedPassword = await bcrypt.hash(user.password, salt); 
+        // Set the hashed password as the new password
+        user.password = hashedPassword; 
+        next(); 
     } catch (error) {
-        return next(error); // Pass the error to the next middleware
+        return next(error); 
     }
 });
+// Create a User model
+const User = mongoose.model('User', userSchema); 
 
-const User = mongoose.model('User', userSchema); // Create a User model using the userSchema
-
-module.exports = User; // Export the User model
+module.exports = User;
